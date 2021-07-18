be1be456cc8505bafc02a090e14192c019e28c9b
hilaolu <i@phyzait.moe>
2021-07-18 13:27:26 +0800

    refactor

--- src/actors/mod.rs
+++ src/actors/mod.rs
@@ -4,86 +4,27 @@ use actix_web::{web,App,HttpRequest,HttpServer};
 use actix_web_actors::ws;
 use std::time::{Duration,Instant};
 use actix::{fut,WrapFuture,ActorFutureExt,ContextFutureSpawner};
-
-
-impl Actor for JudgerWs{
-    type Context=ws::WebsocketContext<Self>;
-    
-    fn started(&mut self, ctx: &mut Self::Context) {
-    
-        let addr = ctx.address(); 
-        self.addr
-            .send(Connect {
-                addr: addr.recipient(),
-                self_id: self.id,
-            })
-            .into_actor(self)
-            .then(|res, _, ctx| {
-                match res {
-                    Ok(_res) => (),
-                    _ => (),
-                }
-                fut::ready(())
-            })
-            .wait(ctx);
-    }
-}
-
-#[derive(Message)]
-#[rtype(result = "()")]
-pub struct Connect {
-    pub addr: Recipient<WsMessage>,
-    pub self_id: Uuid,
-}
-
-pub struct JudgerWs {
-    hb: Instant,
-    id:Uuid,
-    addr:Addr<Judgers>
-}
-
-impl JudgerWs{
-    pub fn new(addr:Addr<Judgers>)->JudgerWs{
-        JudgerWs{
-            hb:Instant::now(),
-            id:Uuid::new_v4(),
-            addr:addr
-        }
-    }
-}
-
-
-
-
-
-impl StreamHandler<Result<ws::Message, ws::ProtocolError>> for JudgerWs {
-    fn handle(
-        &mut self,
-        msg: Result<ws::Message, ws::ProtocolError>,
-        ctx: &mut Self::Context,
-    ) {
-        match msg {
-            Ok(ws::Message::Ping(msg)) => ctx.pong(&msg),
-            Ok(ws::Message::Text(text)) => ctx.text(text),
-            Ok(ws::Message::Binary(bin)) => ctx.binary(bin),
-            _ => (),
-        }
-    }
-}
-
+use uuid::Uuid;
+use std::collections::HashMap;
+use actix::Addr;
+use tokio::time::{self};
+use actix_web::web::Data;
 use actix::prelude::{Context, Handler, Recipient,Message};
 use actix::{AsyncContext};
 
 type Socket=Recipient<WsMessage>;
 
-#[derive(Message)]
-#[rtype(result="()")]
-pub struct WsMessage(pub String);
+mod judgerws;
+mod messages;
+
+pub use judgerws::*;
+pub use messages::*;
+
+
 
 pub struct Judgers{
     sessions:HashMap<Uuid,Socket>,
 }
-
 
 impl Default for Judgers{
     fn default()->Judgers{
@@ -93,11 +34,6 @@ impl Default for Judgers{
     }
 }
 
-use uuid::Uuid;
-use std::collections::HashMap;
-use actix::Addr;
-use tokio::time::{self};
-use actix_web::web::Data;
 
 pub async fn call_back(
     srv: Data<Addr<Judgers>>
@@ -157,10 +93,3 @@ impl Judgers {
 
 }
 
-impl Handler<WsMessage> for JudgerWs {
-    type Result = ();
-
-    fn handle(&mut self, msg: WsMessage, ctx: &mut Self::Context) {
-        ctx.text(msg.0);
-    }
-}
