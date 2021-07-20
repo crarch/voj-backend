fe0c17b086778b10f297a2d078d3ee683285a92b
hilaolu <i@phyzait.moe>
2021-07-21 01:20:13 +0800

    refactor

--- src/models/mod.rs
+++ src/models/mod.rs
@@ -2,11 +2,9 @@ pub mod user;
 pub mod user_id;
 pub mod pass;
 pub mod testbench;
-pub mod queue;
 pub mod record;
 
 pub use pass::*;
-pub use queue::*;
 pub use testbench::*;
 pub use user_id::UserId;
 pub use record::*;
