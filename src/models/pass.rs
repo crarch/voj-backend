e5b2b02be32eb571b1b40d00bfff688dd9d13116
hilaolu <i@phyzait.moe>
2021-10-12 00:49:40 +0800

    update

--- src/models/pass.rs
+++ src/models/pass.rs
@@ -10,7 +10,7 @@ pub async fn query_pass_by_id(mongo:MongoDB,user_id:u32)->Result<Document,()>{
     let cursor=collection.find_one(
         doc!{"user_id":user_id},
         mongodb::options::FindOneOptions::builder()
-            .projection(Some(doc!{"pass":1}))
+            .projection(Some(doc!{"pass":1,"_id":0}))
             .build()
     ).await.unwrap();
 
