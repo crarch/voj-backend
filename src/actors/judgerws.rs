391d9107e60fdc3c25e7f8d9c1ee5c3e66ebd98b
hilaolu <i@phyzait.moe>
2021-12-03 12:35:02 +0800

    update

--- src/actors/judgerws.rs
+++ src/actors/judgerws.rs
@@ -1,29 +1,28 @@
-use actix::{Actor,StreamHandler,ActorContext,Running};
-use actix_web_actors::ws;
-use actix::{fut,WrapFuture,ActorFutureExt,ContextFutureSpawner};
-use uuid::Uuid;
+use actix::prelude::Handler;
 use actix::Addr;
-use actix::prelude::{Handler};
-use actix::{AsyncContext};
+use actix::AsyncContext;
+use actix::{fut, ActorFutureExt, ContextFutureSpawner, WrapFuture};
+use actix::{Actor, ActorContext, Running, StreamHandler};
+use actix_web_actors::ws;
+use log::warn;
 use std::time::{Duration, Instant};
-use log::{debug, error, log_enabled, info, Level,warn};
+use uuid::Uuid;
 
-use super::WsJob;
-use super::WsJudgeResult;
 use super::Connect;
+use super::Disconnect;
 use super::Job;
 use super::Queue;
-use super::Disconnect;
+use super::WsJob;
+use super::WsJudgeResult;
 
+impl Actor for JudgerWs {
+    type Context = ws::WebsocketContext<Self>;
 
-impl Actor for JudgerWs{
-    type Context=ws::WebsocketContext<Self>;
-    
     fn started(&mut self, ctx: &mut Self::Context) {
         self.hb(ctx);
-    
-        warn!("Judger {} Connection Established",&self.id);
-        let addr = ctx.address(); 
+
+        warn!("Judger {} Connection Established", &self.id);
+        let addr = ctx.address();
         self.queue_addr
             .send(Connect {
                 addr: addr.recipient(),
@@ -39,37 +38,34 @@ impl Actor for JudgerWs{
             })
             .wait(ctx);
     }
-    
+
     fn stopping(&mut self, _: &mut Self::Context) -> Running {
-        warn!("Judger {} Disconnecting",self.id);
+        warn!("Judger {} Disconnecting", self.id);
         self.queue_addr.do_send(Disconnect { id: self.id });
         Running::Stop
     }
 }
 
 pub struct JudgerWs {
-    hb:Instant,
-    id:Uuid,
-    queue_addr:Addr<Queue>,
+    hb: Instant,
+    id: Uuid,
+    queue_addr: Addr<Queue>,
 }
 
-impl JudgerWs{
-    pub fn new(
-        queue_addr:Addr<Queue>,
-    )->JudgerWs{
-        JudgerWs{
-            hb:Instant::now(),
-            id:Uuid::new_v4(),
-            queue_addr:queue_addr
+impl JudgerWs {
+    pub fn new(queue_addr: Addr<Queue>) -> JudgerWs {
+        JudgerWs {
+            hb: Instant::now(),
+            id: Uuid::new_v4(),
+            queue_addr: queue_addr,
         }
     }
-    
 }
 
 const HEARTBEAT_INTERVAL: Duration = Duration::from_secs(2);
 const CLIENT_TIMEOUT: Duration = Duration::from_secs(5);
 
-impl JudgerWs{
+impl JudgerWs {
     fn hb(&self, ctx: &mut ws::WebsocketContext<Self>) {
         ctx.run_interval(HEARTBEAT_INTERVAL, |act, ctx| {
             if Instant::now().duration_since(act.hb) > CLIENT_TIMEOUT {
@@ -77,38 +73,34 @@ impl JudgerWs{
                 ctx.stop();
                 return;
             }
-                
+
+            ctx.ping(b"hihi");
             ctx.ping(b"hihi");
         });
     }
 }
 
-
 impl StreamHandler<Result<ws::Message, ws::ProtocolError>> for JudgerWs {
-    fn handle(
-        &mut self,
-        msg: Result<ws::Message, ws::ProtocolError>,
-        ctx: &mut Self::Context,
-    ) {
+    fn handle(&mut self, msg: Result<ws::Message, ws::ProtocolError>, ctx: &mut Self::Context) {
         match msg {
             Ok(ws::Message::Ping(msg)) => {
                 self.hb = Instant::now();
                 ctx.pong(&msg);
-            },
+            }
             Ok(ws::Message::Pong(_)) => {
                 self.hb = Instant::now();
-            },
+            }
             Ok(ws::Message::Close(reason)) => {
                 ctx.close(reason);
                 ctx.stop();
-            },
-            Ok(ws::Message::Text(text)) =>{
+            }
+            Ok(ws::Message::Text(text)) => {
                 self.hb = Instant::now();
-                let judge_result:Job=serde_json::from_str(&text).unwrap();
+                let judge_result: Job = serde_json::from_str(&text).unwrap();
                 self.queue_addr.do_send(WsJudgeResult(judge_result));
-            }, 
+            }
             unexpected => {
-                println!("{:?}",unexpected);
+                println!("{:?}", unexpected);
                 panic!()
             }
         }
@@ -119,8 +111,8 @@ impl Handler<WsJob> for JudgerWs {
     type Result = ();
 
     fn handle(&mut self, job: WsJob, ctx: &mut Self::Context) {
-        let WsJob(job)=job;
-        let job=serde_json::to_string(&job).unwrap();
+        let WsJob(job) = job;
+        let job = serde_json::to_string(&job).unwrap();
         ctx.text(job);
     }
 }
